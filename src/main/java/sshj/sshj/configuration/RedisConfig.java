dc963ae32ffaa3117e28530ddb837618b2875bbf
krimson701 <krimson701@gmail.com>
2020-09-03 14:05:33 +0900

    설정 수정

--- src/main/java/sshj/sshj/configuration/RedisConfig.java
+++ src/main/java/sshj/sshj/configuration/RedisConfig.java
@@ -1,10 +1,8 @@
 package sshj.sshj.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,11 +10,8 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Configuration
 @EnableRedisRepositories
-@Profile("prod")
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
