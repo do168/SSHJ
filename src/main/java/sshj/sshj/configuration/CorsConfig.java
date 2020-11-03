f867b2ea18fd3e690c2d414ffe4936bc77852c7f
krimson701 <krimson701@gmail.com>
2020-11-04 00:24:53 +0900

    cors 수정

--- src/main/java/sshj/sshj/configuration/CorsConfig.java
+++ src/main/java/sshj/sshj/configuration/CorsConfig.java
@@ -1,38 +1,18 @@
 package sshj.sshj.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig extends WebMvcConfigurationSupport {
+public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry cr) {
 		cr.addMapping("/**").allowedOrigins("*").allowedOrigins("http://localhost:9090") // 허용할 주소 및 포트
-				.allowCredentials(true).allowedHeaders("*");
+				.allowCredentials(true).allowedHeaders("*").allowedMethods("DELETE", "POST", "GET", "OPTIONS");
 		// allowed method 는 default로 GET, POST, DELETE 만 설정 PUT을 직접넣어줘야함
 	}
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        // Allow anyone and anything access. Probably ok for Swagger spec
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
