a177b88ec2c6fe6cef15d71f93d95ebfeeef0ca0
krimson701 <krimson701@gmail.com>
2020-11-24 12:45:14 +0900

    모델 hidden 수정

--- src/main/java/sshj/sshj/dto/UserInfoModel.java
+++ src/main/java/sshj/sshj/dto/UserInfoModel.java
@@ -6,11 +6,11 @@ import lombok.Data;
 @Data
 public class UserInfoModel {
 
+    @ApiModelProperty(value = "포탈이메일", required = true)
+    private String email;
+	
     @ApiModelProperty(value = "패스워드", required = true)
     private String password;
-
-    @ApiModelProperty(value = "포탈이메일", required = false, hidden = true)
-    private String email;
 
     @ApiModelProperty(value = "닉네임", required = true)
     private String nickname;
