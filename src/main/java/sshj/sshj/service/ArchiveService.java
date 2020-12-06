1785a3e30bc54d995342ffc8e7ca5df9a3ea342c
krimson701 <krimson701@gmail.com>
2020-12-06 15:34:12 +0900

    Auto stash before merge of "feature/archive" and "origin/feature/archive"

--- src/main/java/sshj/sshj/service/ArchiveService.java
+++ src/main/java/sshj/sshj/service/ArchiveService.java
@@ -3,12 +3,12 @@ package sshj.sshj.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
 import sshj.sshj.dto.SimpleFileDto;
 
 /**
  * 아카이브 (파일) 서비스
  * @author krims
- *
  */
 @Service
 public interface ArchiveService {
