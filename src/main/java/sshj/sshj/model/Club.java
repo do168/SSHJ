58a102aeda6eae4da9d3a9d0ce111c612602c562
do1235580 <do1235580@gmail.com>
2021-04-14 01:00:09 +0900

    refactor: add createdAt field & add toString func

--- src/main/java/sshj/sshj/model/Club.java
+++ src/main/java/sshj/sshj/model/Club.java
@@ -1,8 +1,11 @@
 package sshj.sshj.model;
 
 import lombok.*;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,12 +16,24 @@ public class Club {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    long id;
+    private long id;
 
     @Column(name = "name")
-    String name;
+    private String name;
 
     @Column(name = "description")
-    String description;
+    private String description;
 
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("id", id)
+                .append("name", name)
+                .append("description", description)
+                .append("createdAt", createdAt)
+                .toString();
+    }
 }
